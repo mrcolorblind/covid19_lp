1653952b0a507d3ed0c24440c00e769e3f890441
João Pedrosa <ja1antoniolp@gmail.com>
2020-04-21 13:43:22 -0300

    update

--- code/graph_generator.py
+++ code/graph_generator.py
@@ -9,12 +9,15 @@ day        = cases['day']
 suspicious = cases['suspicious']
 positive   = cases['positive']
 negative   = cases['negative']
-date       = [(str(year[i]) + "-" + str(month[i]) + "-" + str(day[i])) for i in range(len(day))]
+date       = [(str(day[i]) + "/" + str(month[i]) + "/" + str(year[i])) for i in range(len(day))]
 index      = range(len(cases))
 
 plt.plot(index, suspicious, '--y', index, negative, '--g', index, positive, '--r')
-plt.title("Casos de COVID-19 em Lagoa da Prata por tempo")
+plt.title("Casos de COVID-19 em Lagoa da Prata por tempo (" + date[len(date)-1] + ")")
 plt.xlabel("Dias desde 22/03 (Primeiro boletim)")
 plt.ylabel("Número de casos")
 plt.legend(["Suspeitos", "Negativos", "Confirmados"])
 plt.savefig("../data/graph.png", dpi=300)
+actual = len(day) - 1
+saveName = "/home/jonas/Imagens/covid/grafico_"+ str(day[actual]) + "_" + str(month[actual]) + "_" + str(year[actual]) + ".png"
+plt.savefig(saveName, dpi=300)
