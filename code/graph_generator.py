23c4f9a5c89859c8b4fddcbced21ab32249a5e24
João Pedrosa <ja1antoniolp@gmail.com>
2020-04-21 13:05:20 -0300

    update

--- code/graph_generator.py
+++ code/graph_generator.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-sns.set()
 
 cases      = pd.read_csv('../data/cases.csv')
 year       = cases['year']
@@ -13,9 +12,9 @@ negative   = cases['negative']
 date       = [(str(year[i]) + "-" + str(month[i]) + "-" + str(day[i])) for i in range(len(day))]
 index      = range(len(cases))
 
-plt.plot(index, suspicious, 'y', index, negative, 'g', index, positive, 'r')
+plt.plot(index, suspicious, '--y', index, negative, '--g', index, positive, '--r')
 plt.title("Casos de COVID-19 em Lagoa da Prata por tempo")
 plt.xlabel("Dias desde 22/03 (Primeiro boletim)")
 plt.ylabel("Número de casos")
 plt.legend(["Suspeitos", "Negativos", "Confirmados"])
-plt.savefig("../data/graph.png")
+plt.savefig("../data/graph.png", dpi=300)
